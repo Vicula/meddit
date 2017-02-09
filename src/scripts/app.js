8bbfedda3ef7b3e3cb947b7e1f9904eeff86c6b5
Victor Carpenter <victorcarpenter123@gmail.com>
2017-02-09 11:02:13 -0500

    got react router working

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,19 @@
-// console.log('Hello World!');
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import axios from 'axios';
 const parseString = require('xml2js').parseString;
+
+import Home from './pages/home.js'
+import Layout from './pages/layout.js'
+
+const app = document.getElementById('mount');
+
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Home}></IndexRoute>
+    </Route>
+  </Router>,
+app)
---
+++ src/scripts/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Home extends React.Component{
+  render(){
+    return (
+      <div className="homePage">
+        <h1>I AM HOME</h1>
+      </div>
+    )
+  }
+}
