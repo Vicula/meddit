e70c0f6677bdbddf5d5f3b0b62856fc570d30df7
Victor Carpenter <victorcarpenter123@gmail.com>
2017-02-09 12:49:55 -0500

    designing the basic layout right now

--- src/scripts/pages/layout.js
+++ src/scripts/pages/layout.js
@@ -4,7 +4,12 @@ export default class Layout extends React.Component{
   render(){
     return (
       <div className="layout">
+        <nav className="navbar navbar-default nav">
+          <span className="fa fa-stethoscope" aria-hidden="true"></span>
+          <h1>Meddit</h1>
+        </nav>
         {this.props.children}
+        <footer></footer>
       </div>
     )
   }
