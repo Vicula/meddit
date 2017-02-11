2529554c253d23243d67544233a12679575db3c7
Victor Carpenter <victorcarpenter123@gmail.com>
2017-02-11 17:49:29 -0500

    pushing up

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 export default class Home extends React.Component{
+  fillBooks(){
+    
+  }
   render(){
     return (
       <div className="homePage">
-        
+
       </div>
     )
   }
